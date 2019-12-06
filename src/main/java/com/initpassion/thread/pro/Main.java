6f3a19640f581d1c5ac9e7cb1175ac1bdbd3e72c
涂圣红 <tushenghong01@corp.netease.com>
2019-12-06 16:24:33 +0800

    maven

---
+++ src/main/java/com/initpassion/thread/pro/Main.java
@@ -0,0 +1,14 @@
+/*
+ * @(#) Main.java 2019-12-06
+ *
+ * Copyright 2019 NetEase.com, Inc. All rights reserved.
+ */
+
+package com.initpassion.thread.pro;
+
+/**
+ * @author tushenghong01
+ * @version 2019-12-06
+ */
+public class Main {
+}
